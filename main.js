13e8727e0613adcb4f5bf74c5ad8b6667c34b790
Caroline Sekel <carolinesekel@Carolines-MacBook-Pro-2.local>
2020-07-19 12:28:49 -0400

    progress on step counter and buttons

--- main.js
+++ main.js
@@ -1,61 +1,36 @@
-var curr = 0
+//initialize
+var $carousel = $('.main-carousel').flickity({
+    // options
+     "cellAlign": "left", 
+     //"autoPlay": true, 
+     "wrapAround": true, 
+     "draggable": true 
+  })
 
-var numSlides = $('.holder div').length
+//flickity data / properties 
+var flkty = $carousel.data('flickity')
+console.log('flkty object: ' + flkty)
 
-var nextSlide = function(){
-  console.log('next slide')
-  curr = curr+1
-  if (curr>=numSlides){
-    curr=0
-  }
-  moveSlide(curr, numSlides)
-}
+var numSlides = flkty.cells.length
+console.log('total slides:' + numSlides)
 
-var previousSlide = function(){
-  console.log('prev slide')
-  curr = curr-1
-  if(curr<0){
-     curr = numSlides-1
-  }
-  moveSlide(curr)
-}
-
-//clean up repeated code into function 
-var moveSlide = function(slide){
-  var leftVal = '-' + slide + '00vw'
-  $('.holder').css('left', leftVal)
-  var slideNum = slide+1
-	$('.steps').text(slideNum + '/' + numSlides)
-}
-
-var autoSlide = setInterval(function(){
-	nextSlide()
-}, 5000)
-
-
-//functionality for next and prev buttons 
-$('.prev').on('click', function(){
-  clearInterval(autoSlide)
-  previousSlide()
-})
-
-$('.next').on('click', function(){
-  clearInterval(autoSlide)
-  nextSlide()
-})
-
-//set text for steps using curr slide at load
-var slideNum = curr+1
+//set text for steps using selected slide at load
+var slideNum = flkty.selectedIndex + 1
+console.log('current slide: ' + slideNum)
 $('.steps').text(slideNum + '/' + numSlides)
 
-//left and right arrows
-$('body').on('keydown', function(event){
-  if(event.keyCode == 37){
-    previousSlide()
-  } 
-  
-  
- if(event.keyCode == 39){
-   nextSlide()
- } 
+//to set text for steps using selected slide after move 
+var updateSteps = function(){
+    var slideNum = flkty.selectedIndex + 1
+    $('.steps').text(slideNum + '/' + numSlides)
+}
+
+$('.next').on('click', function(){
+    $carousel.flickity('next')
+    updateSteps()
+})
+
+$('.prev').on('click', function(){
+    $carousel.flickity('previous')
+    updateSteps()
 })
