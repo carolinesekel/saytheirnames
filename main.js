af5b87688fabbaf82da707f768fa253fbf922d1f
Caroline Sekel <carolinesekel@carolines-mbp-2.lan>
2020-07-31 17:49:26 -0400

    removed step counter to stick with simpler flickity components, added two names

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var $carousel = $('.main-carousel').flickity({
     // options
      "cellAlign": "left", 
-     //"autoPlay": true, 
+     "autoPlay": true, 
      "wrapAround": true, 
      "draggable": true 
   })
@@ -15,9 +15,9 @@ var numSlides = flkty.cells.length
 console.log('total slides:' + numSlides)
 
 //set text for steps using selected slide at load
-var slideNum = flkty.selectedIndex +1
+/*var slideNum = flkty.selectedIndex +1
 console.log('current slide: ' + slideNum)
-$('.steps').text(slideNum + '/' + numSlides)
+$('.steps').text(slideNum + '/' + numSlides)*/
 
 //to set text for steps using selected slide after move 
 /*var updateSteps = function(){
