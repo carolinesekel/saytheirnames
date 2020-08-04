238228ecc3c5de3c344c77a2733f0c687fdd0ef0
Caroline Sekel <carolinesekel@carolines-mbp-2.lan>
2020-08-03 22:12:03 -0400

    custom cursor

--- main.js
+++ main.js
@@ -3,34 +3,6 @@ var $carousel = $('.main-carousel').flickity({
     // options
      "cellAlign": "left", 
      "autoPlay": true, 
-     "wrapAround": true, 
-     "draggable": true 
+     "wrapAround": true,
+     "draggable":false
   })
-
-//flickity data / properties 
-var flkty = $carousel.data('flickity')
-console.log('flkty object: ' + flkty)
-
-var numSlides = flkty.cells.length
-console.log('total slides:' + numSlides)
-
-//set text for steps using selected slide at load
-/*var slideNum = flkty.selectedIndex +1
-console.log('current slide: ' + slideNum)
-$('.steps').text(slideNum + '/' + numSlides)*/
-
-//to set text for steps using selected slide after move 
-/*var updateSteps = function(){
-    var slideNum = flkty.selectedIndex +1
-    $('.steps').text(slideNum + '/' + numSlides)
-}
-
-$('.next').on('click', function(){
-    $carousel.flickity('next')
-    updateSteps()
-})
-
-$('.prev').on('click', function(){
-    $carousel.flickity('previous')
-    updateSteps()
-})*/
